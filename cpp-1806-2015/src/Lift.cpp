c85b91e8b7bdca3633be7e6b49434e3972215511
TheGuyWhoCodes <christopherariagno@gmail.com>
2015-06-24 17:27:10 -0500

    most of the lift setup
    need to find out the pwm ports for everything. also i have no idea if
    this works or not, can’t even built the thing

--- cpp-1806-2015/src/Lift.cpp
+++ cpp-1806-2015/src/Lift.cpp
@@ -5,15 +5,24 @@
  *      Author: Dillon
  */
 
+/* TODO
+ * FIND OUT ALL THE PWM PORTS.
+ */
+
 #include <Lift.h>
 #include <TDPIDController.h>
-
+#include <WPIlib.h>
 Lift::Lift() {
 	// TODO Auto-generated constructor stub
-
+	elevEnc = new Encoder(1,2); //I DONT KNOW THE PORTS
+	topLimit = new DigitalInput(1,2); //top limit switch
+	elevMotor = new Talon(1,2); //elevator motor
+	brake = new DoubleSolenoid(1,2); //double solennoid
+	bottomLimit = new DigitalInput(1,2); //bottom limit switch
+	photoSensor = new DigitalInput(1,2); //photo sensor for totes
 }
 
 Lift::~Lift() {
-	// TODO Auto-generated destructor stub
+
 }
 
--- cpp-1806-2015/src/Lift.h
+++ cpp-1806-2015/src/Lift.h
@@ -11,7 +11,22 @@
 class Lift {
 public:
 	Lift();
+	double getElevEncDist(); 	//Elevator Encoder
+	bool isBrakeEngaged(); 	//Brake on or off
+	double getElevMotorPower();   //What percentage of power
+	bool isElevStopped(); //Is there elevator stopped?
+	void setElevMotorPower(); //Setting the Elevator motor power. i think i set it up wrong whoops
+
 	virtual ~Lift();
+private:
+	Encoder* elevEnc;
+	DigitalInput *topLimit; //top limit switch
+	Talon *elevMotor; //THE MOTOR :))))
+	DoubleSolenoid *brake;  //BRAKE TO STOP THE LIFT
+	DigitalInput *bottomLimit; // bottom limit switch
+	DigitalInput *photoSensor; //tote photosensor
+
+
 };
 
 #endif /* SRC_LIFT_H_ */
