b2e23434b58a5c1f9a6a24c594d354d95030f658
JasonBateman <batemanj@stu.smithville.k12.mo.us>
2015-06-30 09:24:36 -0500

    Jason-Stax Class
    Jason implements his Stax class. Read the interface for more information
    :dancer:

--- cpp-1806-2015/src/Stax.cpp
+++ cpp-1806-2015/src/Stax.cpp
@@ -1,13 +1,14 @@
 /*
  * Stax.cpp
  *
- *  Created on: Jun 19, 2015
- *      Author: Dillon
+ *  Created on: Jun 21, 2015
+ *      Author: Jason
  */
 
-#include <Stax.h>
+#include "Stax.h"
 #include "Lift.h"
 #include "Arms.h"
+#include "WPILib.h"
 
 Stax::Stax() {
 	// TODO Auto-generated constructor stub
@@ -18,3 +19,68 @@ Stax::~Stax() {
 	// TODO Auto-generated destructor stub
 }
 
+void goTo(double encValue, bool Slow){
+	while (!isRested()){
+		/*
+		 * insert the PID code here for the elevator. I know, right.
+		 * This sounds like a lot
+		 *
+		 * it is
+		 * 10/10
+		 *
+		 * I can make Arduino write his own PID
+		 */
+	}
+}
+
+bool isRested(){
+	/*
+	 * This function checks
+	 * 1. The Velocity of the Elevator
+	 * 2. The Acceleration of the Elevator
+	 *
+	 */
+	return true;
+}
+
+void selectMode(){
+	/*
+	 * I was thinking that this would be a whole bunch of
+	 * if statements that determine what mode the elevator
+	 * will be in.
+	 */
+}
+
+int getMode(){
+	return Stax::currentMode;
+}
+
+void pickUpTote(){
+	/*
+	 * This is going to be a rough function right here.
+	 * But It will encompass the process of picking
+	 * up the totes from the floor to the second stage.
+	 * It will be a state machine tho!
+	 */
+}
+
+void dropTotes(){
+	/*
+	 * Another state machine
+	 * This one will INTERUPT the function pickUpTote()
+	 * Just like in the old code
+	 */
+}
+
+void setIntakeSpeed(Talon motor, int speed){
+	if((speed>100)||(speed<100)){
+		motor->Set(speed); //Ask Dillon. This needs help
+	}
+	else {
+		printf("Intake Speed Invalid"); //look this function up. cplusplus.com isn't helping
+	}
+}
+
+void goingUp(){
+
+}
--- cpp-1806-2015/src/Stax.h
+++ cpp-1806-2015/src/Stax.h
@@ -1,17 +1,51 @@
 /*
  * Stax.h
  *
- *  Created on: Jun 19, 2015
- *      Author: Dillon
+ *  Created on: Jun 21, 2015
+ *      Author: Jason
  */
+#include "WPILib.h"
+#include <Lift.h>
+#include <Arms.h>
 
 #ifndef SRC_STAX_H_
 #define SRC_STAX_H_
 
 class Stax {
 public:
+	double SECOND_STAGE_ENC_VALUE = 13.9; //Pretty self-explanatory
+	double PAUSED_ENC_VALUE = 3.5; //The intermediate value for the elevator's interrupted stage
+
 	Stax();
 	virtual ~Stax();
+
+	void goTo(double encValue, bool slow=false); // This sends the elevator to a location on the elevator.
+	bool isRested(); //checks to see if the elevator is at rest
+	void selectMode( );
+	/* I think that this is the function that will
+	 * change the mode that we are in based on joystick inputs and completion of different
+	 * processes.
+	 */
+	int getMode();
+	void pickUpTote();
+	/* This is the process of picking up the tote from the ground to the second stage
+	 * I hope that this process is so much smoother than the LabVIEW version
+	 *
+	 */
+	void dropTotes(); //previously known as the DROP SEQUENCE :D
+	void setIntakeSpeed(Talon motor, int Speed);
+	void getIntakeSpeed();
+private:
+	enum mode {
+		autoCollect,
+		manualMode,
+		reset,
+		pauseFunction
+	} currentMode;	//This enum is to determine the current state of things with the robot.
+	bool goingUp; //checks to see if the elevator is going up
+	int intakeSpeed;
+
+
 };
 
 #endif /* SRC_STAX_H_ */
