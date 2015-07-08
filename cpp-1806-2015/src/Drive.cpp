2d93ce71db34a7571fe46df681cce39819efb21b
JasonBateman <batemanj@stu.smithville.k12.mo.us>
2015-07-08 10:50:42 -0500

    7-8-15 By Gaben(Gavin) and Jason
    Added in new drive of 45% and added in deadzone.

--- cpp-1806-2015/src/Drive.cpp
+++ cpp-1806-2015/src/Drive.cpp
@@ -12,12 +12,14 @@
 Drive::Drive() {
 	// TODO Auto-generated constructor stub
 	leftEnc = new Encoder(1,2);
-	leftEnc -> SetDistancePerPulse(0.212765957)
+	leftEnc -> SetDistancePerPulse(0.212765957);
 	rightEnc = new Encoder(3,4);
-	rightEnc -> SetDistancePerPulse(0.212765957)
+	rightEnc -> SetDistancePerPulse(0.212765957);
 	rDrive = new Talon(1);
 	lDrive = new Talon(2);
 	gyro = new Gyro(0);
+	bool slowSpeed = true;
+
 	Joystick stick;
 	TDButton *Button1;
 	TDButton *Button2;
@@ -67,7 +69,33 @@ double Drive::getGyroAngle(){
 	return gyro->GetAngle();
 }
 
+void setSlow(bool slow){
+	Drive.slowSpeed = slow;
+}
 void Drive::arcadeDrive(Joystick stick){
-	rDrive->Set(stick.GetRawAxis(2)-stick.GetRawAxis(4));
-	lDrive->Set(stick.GetRawAxis(2)+stick.GetRawAxis(4));
+	double power = 0;	 //This is the power variable based on the left y axis
+	double turn = 0;	 //This is the turn variable based on right x axis
+if((stick.GetRawAxis(2) < -.1 )||(stick.GetRawAxis(2) > .1)){ //Deadzone for the left y axis
+	power = .45*stick.GetRawAxis(2);
+	printf("The Thrust is " + power);
+}
+else {
+	power = 0;
+}
+if((stick.GetRawAxis(4) < -.1 )||(stick.GetRawAxis(4) > .1)){ //Deadzone for the right x axis
+	turn = .45*stick.GetRawAxis(4);
+	printf("The Turn is " + turn);
+}
+else {
+	turn = 0;
+}
+
+if (Drive.slowSpeed){
+	printf("Speed is slowed");
+	power = power*.45/.75;
+	turn = turn*.45/.75;
+}
+rDrive = (power - turn);
+lDrive = (power + turn);
+}
 }
--- cpp-1806-2015/src/Drive.h
+++ cpp-1806-2015/src/Drive.h
@@ -6,19 +6,20 @@
  */
 #ifndef SRC_DRIVE_H_
 #define SRC_DRIVE_H_
-#include <WPILib.h>
 
 class Drive {
 public:
 	Drive();
-	virtual ~Drive()
+	virtual ~Drive();
 	Joystick stick;
 	double getEncDistance(Encoder* encoder);		//reads where the encoder is rn
 	void resetEnc(Encoder* encoder);				//resets the encoder values
 	void setSpeed(Talon* motor, int speed);		//sets the speed for a motor
 	double getSpeed(Talon* motor);				//returns the speed of motor
 	double getGyroAngle();						//returns value of gryo angle
+	void setSlow(bool slow);					//Returns the decision to go slow
 	void arcadeDrive(Joystick stick);					//the function that will actually drive
+
 
 
 private:
@@ -27,6 +28,7 @@ private:
 	Talon* rDrive;
 	Talon* lDrive;
 	Gyro* gyro;
+	bool slowSpeed;
 };
 
 #endif /* SRC_DRIVE_H_ */
